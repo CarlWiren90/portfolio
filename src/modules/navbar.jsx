290bbc583543a8bb478cba47a8ddf0291edc0908
CarlWiren90 <156791432+CarlWiren90@users.noreply.github.com>
2024-12-08 12:21:48 +0100

    Remade navbar

--- src/modules/navbar.jsx
+++ src/modules/navbar.jsx
@@ -6,18 +6,14 @@ const Navbar = () => {
       <>
         <nav className="navbar">
             <div className="navbar__wrapper">
-                <a className="navbar__link" href="#">Experience</a>
-                <a className="navbar__link" href="#">Projects</a>
-            </div>
-
-       {/*      <div className="navbar__wrapper--logo">
+                <span className="navbar__link navbar__status"><span className="weight">Status:</span> Looking for internship</span>
                 <a className="navbar__link" href="https://www.github.com/CarlWiren90" rel="noopener noreferrer" target="_blank" aria-label="Go to Github profile">
                     <img className="navbar__logo" src="/GitHub.avif" alt="GitHub logo"/>
                 </a>
                 <a className="navbar__link" href="https://www.linkedin.com/in/carl-wirén-36990b330" rel="noopener noreferrer" target="_blank" aria-label="Go to Linked in profile">
                     <img className="navbar__logo" src="/LinkedIn.avif" alt="Linked in logo"/>
                 </a>
-            </div>   */}
+            </div>
         </nav>
       </>  
     )
