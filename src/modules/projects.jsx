6bd29036a2198f74063c25487b9470464b29a59f
CarlWiren90 <156791432+CarlWiren90@users.noreply.github.com>
2024-12-10 09:37:37 +0100

    Update on projectviewer

--- src/modules/projects.jsx
+++ src/modules/projects.jsx
@@ -3,8 +3,13 @@ import React from 'react';
 import { projectsList } from './projectsList';
 
 const Projects = () => {
-    
 
+    const displayProject = (project) => {
+        const image = event.target;
+/*         image.classList.toggle('displayed')
+ */        console.log(event.target);
+    }
+    
     return (
         <>
             <section className="projects">
@@ -12,14 +17,22 @@ const Projects = () => {
                     <h1 className="content__heading">Projects.</h1>
                 </section>
 
-                <section className="content__info-container content__info-container--skillset">
-                    <h2 className="content__info">
-    
-                    </h2>  
+                <section className="content__info-container content__info-container--projects">
+                    {projectsList.map((project, index) => {
+                        return (
+                        <div key={index} className='image__container' onClick={() => displayProject(project)}>
+                            <img key={index} className='project__image' src={project.projectImg}>
+                         </img>
+                        </div>
+
+
+                        )
+                    })}
                 </section>
             </section>
         </>
     );
 }
+
 
 export default Projects;
